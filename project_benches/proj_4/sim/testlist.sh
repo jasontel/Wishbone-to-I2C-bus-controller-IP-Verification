b48d8a5099136e8d0875a520610dc02700e8932c
TC <jason081058@gmail.com>
2024-04-11 22:29:14 -0400

    remove rand_test

--- project_benches/proj_4/sim/testlist.sh
+++ project_benches/proj_4/sim/testlist.sh
@@ -4,4 +4,4 @@ make run_cli GEN_TYPE=i2cmb_generator_register_test
 make run_cli GEN_TYPE=i2cmb_generator_fsm_functionality_test
 make run_cli GEN_TYPE=i2cmb_generator_control_functionality_test
 make run_cli GEN_TYPE=i2cmb_generator_direct_test
-make run_cli GEN_TYPE=i2cmb_generator_random_test TEST_SEED=100
+# make run_cli GEN_TYPE=i2cmb_generator_random_test TEST_SEED=100
