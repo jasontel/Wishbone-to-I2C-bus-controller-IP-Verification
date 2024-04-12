6206450e3ef51d2cbb455a543b2f16e5c09afe5c
TC <jason081058@gmail.com>
2024-04-11 22:34:06 -0400

    remove direct_test

--- project_benches/proj_4/sim/testlist.sh
+++ project_benches/proj_4/sim/testlist.sh
@@ -3,5 +3,5 @@ make compile
 make run_cli GEN_TYPE=i2cmb_generator_register_test
 make run_cli GEN_TYPE=i2cmb_generator_fsm_functionality_test
 make run_cli GEN_TYPE=i2cmb_generator_control_functionality_test
-make run_cli GEN_TYPE=i2cmb_generator_direct_test
+# make run_cli GEN_TYPE=i2cmb_generator_direct_test
 # make run_cli GEN_TYPE=i2cmb_generator_random_test TEST_SEED=100
